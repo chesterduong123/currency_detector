373516a9accfd1ed157ade5c2193daf54a7551ff
chesterduong123 <chesterduong123@gmail.com>
2026-06-22 18:08:16 -0700

    new runs

--- train_money.py
+++ train_money.py
@@ -4,22 +4,19 @@ if __name__ == "__main__":
     # For a new model, use:
     # model = YOLO("yolo26n.pt")
 
-    model = YOLO(r"C:\Users\chest\OneDrive\Documents\YOLO26 Stuff\Currency Project\runs\detect\train-25s960r\weights\last.pt")
+    model = YOLO(r"C:\Users\chest\OneDrive\Documents\YOLO26 Stuff\Currency Project\runs\detect\train-30m640r\weights\best.pt")
     model.train(
         data=r"C:\Users\chest\OneDrive\Documents\YOLO26 Stuff\Currency Project\data.yaml",
-        epochs=10,
-        imgsz=960,
-        device=0,
-        workers=0,
-        scale=0.5,
-        dropout=0.1,
-        degrees=15.0,
-        perspective=0.001,
-        mixup=0.1,
-        copy_paste=0.1,
-        batch=8,
-        # pretrained=r"C:\Users\chest\OneDrive\Documents\YOLO26 Stuff\Currency Project\runs\detect\train-150 epochs\weights\best.pt",
-        patience=5,
-        cache='disk',
-        verbose=False
+        epochs=30,           # Train longer, let patience handle the stopping
+        # patience=20,
+        imgsz=640,            # Start at 640; it's faster and more stable than 960
+        batch=-1,             # Let AutoBatch optimize memory
+        workers=4,            # Feed the GPU faster
+        cache='disk'
+        # degrees=15.0,
+        # scale=0.5,
+        # dropout=0.1,
+        # perspective=0.001,
+        # mixup=0.0,
+        # copy_paste=0.0
     )
